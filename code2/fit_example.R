54fc091fdd80a920b8905972c3e2032f086c14d9
PangChung <zhongprw@gmail.com>
2024-06-05 02:07:53 +1000

    update

--- code2/fit_example.R
+++ code2/fit_example.R
@@ -17,39 +17,11 @@ x.coord <-  y.coord <- c(1:d)/(d+1) #grids
 coord = as.matrix(expand.grid(x.coord,y.coord))
 Sigma = exp(-as.matrix(dist(coord))/1)*parR[3]
 
-reg=cbind(1,2*pnorm(coord[,1],0.5,0.25)-1)
-reg.t = 0
-
-# data.approx <- mcmapply(rmaxidspat,n=rep(1,n),MoreArgs = list(coord=as.matrix(dist(coord)),parR=parR,parGauss = parGauss,reg=NULL,reg.t=NULL),SIMPLIFY = T,mc.cores = ncores)
-
 data.mh <- apply(mcmapply(mh,n=rep(1,n),SIMPLIFY = T,mc.cores = ncores,MoreArgs = list(parR=parR,Sigma=Sigma)),1,pG,parR=parR)
 hist(data.mh[,100])
 pairs <- t(combn(ncol(Sigma),2))
 
 emp.pair(1000,pairs,0.9,-1/log(data.mh),parR=parR,cor.mat=Sigma/parR[3])
-
-# data.ars <- apply(mcmapply(mh,n=rep(1,n),MoreArgs = list(ars=T,parR=parR,Sigma=Sigma),SIMPLIFY = T,mc.cores = ncores),1,pG,parR=parR)
-
-fixed <- c(F,F,F,F,T,F) ## corresponding to c(alpha, beta, lambda.0,lambda.1,lambda.2,nu)
-init <- c(pars$parR,pars$parGauss$lambda,pars$parGauss$lambda.t,pars$parGauss$nu)
-init[!fixed] = init[!fixed] + runif(sum(!fixed),0.1,0.3)
-init[1:2] <- log(init[1:2])
-
-
-
-
-### Fit the model. It will take hours. ###
-# message("Fit the model for ARS")
-# fit.ars <- fit.pw.parallel(init=init,datU = u.ars,coord=coord,reg=reg,reg.t=NULL,cutoff=cutoff,proppairs= 1,fixed=fixed,optim =T, hessian=F,sandwich=F,eps = 10^(-2), print.par.file=NULL,ncores=ncores,fit.load=F, fit.save=F,fit.file=NULL)
-
-# message("Fit the model for Metropolis Hastings")
-# fit.mh <- fit.ars <- fit.pw.parallel(init=init,datU = u.mh,coord=coord,reg=reg,reg.t=NULL,cutoff=cutoff,proppairs= 1,fixed=fixed,optim =T, hessian=F,sandwich=F,eps = 10^(-2), print.par.file=NULL,ncores=ncores,fit.load=F, fit.save=F,fit.file=NULL)
-
-# message("Fit the model for The Approx")
-# fit.approx <- fit.pw.parallel(init=init,datU = u.approx,coord=coord,reg=reg,reg.t=NULL,cutoff=cutoff,proppairs= 1,fixed=fixed,optim =T, hessian=F,sandwich=F,eps = 10^(-2), print.par.file=NULL,ncores=ncores,fit.load=F, fit.save=F,fit.file=NULL)
-
-# save.image(paste0("fit_simu_examples/fit_simu_",index,".RData"))
-
 
 
 
